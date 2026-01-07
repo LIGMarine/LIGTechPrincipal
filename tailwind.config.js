c0ab26098f34c815150a9c8541c1c804833552d4
Mark Greenway <MarkGreenwayIT@LIGMarine.com>
2026-01-06 22:20:37 -0500

    Dark mode and favicons

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}',
+    './public/**/*.{html,js}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          // Blues
+          primary: '#004DA9', // deep navy-blue
+          primaryLight: '#5A88B5',
+          primaryDark: '#050076',
+          // Oranges
+          accent: '#F36424', // vivid orange
+          accentLight: '#E58B61',
+          accentDark: '#DE6B35',
+          // Backgrounds
+          lightBg: '#D6DEF4',
+          darkBg: '#5F5F5F',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
